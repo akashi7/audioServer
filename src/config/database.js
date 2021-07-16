10d415ac3b0a8b9071f1c7dd15069dd661d19f41
akashi7 <christiannseko@gmail.com>
2021-07-17 00:40:38 +0200

    initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -4,19 +4,19 @@ const { config } = require('dotenv');
 config();
 
 
-// const db = createPool({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD
-// });
-
 const db = createPool({
-  host: "localhost",
-  user: "root",
-  database: "musicapp",
-  password: "Akashikabuto7"
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD
 });
+
+// const db = createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "musicapp",
+//   password: "Akashikabuto7"
+// });
 
 module.exports = db;
 
